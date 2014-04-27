026c83bf0c77eac09a8e652a9b1f69465764b1f5
Tobias Reich <tobias.reich.ich@gmail.com>
2014-04-27 16:02:29 +0200

    Added setPayment, getID to album/photo and made some code adjustments

--- php/modules/Album.php
+++ php/modules/Album.php
@@ -1,33 +1,64 @@
 <?php
 
-/**
- * @name		Album Module
- * @author		Tobias Reich
- * @copyright	2014 by Tobias Reich
- */
+###
+# @name			Album Module
+# @author		Tobias Reich
+# @copyright	2014 by Tobias Reich
+###
 
 class Album {
 
-	private $database = null;
-	private $albumID = null;
+	private $database	= null;
+	private $albumID	= null;
 
-	function __construct($database, $albumID) {
+	public function __construct($database, $albumID) {
 
-		$this->database = $database;
-		$this->albumID = $albumID;
+		$this->database	= $database;
+		$this->albumID	= $albumID;
 
 	}
 
-	function getUserID() {
+	public function getID() {
+
+		if (!isset($this->albumID)) return false;
+		return $this->albumID;
+
+	}
+
+	public function getUserID() {
 
 		if (!isset($this->database, $this->albumID)) exit('Error: Database or albumID missing');
 
 		$query	= "SELECT * FROM lychee_albums WHERE id = '$this->albumID' LIMIT 1;";
-		$result = $this->database->query($query);
-		$return = $result->fetch_assoc();
+		$result	= $this->database->query($query);
+		$return	= $result->fetch_assoc();
 
 		if (!isset($return['title'])) exit('Error: Album title not found');
 		else return substr($return['title'], 0, 2);
+
+	}
+
+	public function setPayment() {
+
+		if (!isset($this->database, $this->albumID)) exit('Error: Database or albumID missing');
+
+		$error	= false;
+
+		$query	= "SELECT id, tags FROM lychee_photos WHERE album = '$this->albumID';";
+		$result	= $this->database->query($query);
+
+		# For each photo
+		while ($photo = $result->fetch_object()) {
+
+			$photoObj	= new Photo($this->database, $photo->id);
+			$setResult	= $photoObj->setPayment();
+
+			if ($setResult!==true) $error = true;
+
+		}
+
+		if ($error===true) return false;
+		return true;
 
 	}
 
--- php/modules/Photo.php
+++ php/modules/Photo.php
@@ -1,35 +1,54 @@
 <?php
 
-/**
- * @name		Photo Module
- * @author		Tobias Reich
- * @copyright	2014 by Tobias Reich
- */
+###
+# @name			Photo Module
+# @author		Tobias Reich
+# @copyright	2014 by Tobias Reich
+###
 
 class Photo {
 
-	private $database = null;
-	private $photoID = null;
+	private $database	= null;
+	private $photoID	= null;
 
-	function __construct($database, $photoID) {
+	public function __construct($database, $photoID) {
 
-		$this->database = $database;
-		$this->photoID = $photoID;
+		$this->database	= $database;
+		$this->photoID	= $photoID;
 
 	}
 
-	function getUserID() {
+	public function getID() {
+
+		if (!isset($this->photoID)) return false;
+		return $this->photoID;
+
+	}
+
+	public function getUserID() {
 
 		if (!isset($this->database, $this->photoID)) exit('Error: Database or photoID missing');
 
 		$query	= "SELECT * FROM lychee_photos WHERE id = '$this->photoID' LIMIT 1;";
-		$result = $this->database->query($query);
-		$return = $result->fetch_assoc();
+		$result	= $this->database->query($query);
+		$return	= $result->fetch_assoc();
 
 		$album		= new Album($this->database, $return['album']);
 		$albumID	= $album->getUserID();
 
 		return $albumID;
+
+	}
+
+	public function setPayment() {
+
+		if (!isset($this->database, $this->photoID)) return false;
+
+		$query	= "UPDATE lychee_photos SET tags = CONCAT(tags, ',payed') WHERE id = $this->photoID;";
+		$result	= $this->database->query($query);
+
+		if (!$result) return false;
+		return true;
 
 	}
 
