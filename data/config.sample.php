f42c21e7ff0fe7bbfcdb98e5151e2c7b11f076f7
Tobias Reich <tobias.reich.ich@gmail.com>
2014-09-02 22:13:35 +0200

    Added sample configuration

---
+++ data/config.sample.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @name		Config
+ * @author		Tobias Reich
+ * @copyright	2014 Tobias Reich
+*/
+
+// Version
+$configVersion = '0.0.1';
+
+// Lychee
+$lychee = '../lychee/'; // Path to the root of Lychee
+
+// Database
+$dbCredentials = array(
+    'host'  => 'localhost', // Host of the Database
+    'user'  => '', // Username of the database
+    'pass'  => '', // Password of the Database
+    'name'  => 'lychee' // Database name
+);
+
+// PayPal
+$apiCredentials = array(
+    'username'  => '',
+    'password'  => '',
+    'signature' => '',
+    'appID'     => ''
+);
+
+?>
